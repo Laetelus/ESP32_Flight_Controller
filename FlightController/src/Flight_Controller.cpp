75abb1abaaf9534edb335f83dd578322ffee0fb5
Laetelus <72906227+Laetelus@users.noreply.github.com>
2023-01-20 02:29:26 -0500

    Test-2

--- FlightController/src/Flight_Controller.cpp
+++ FlightController/src/Flight_Controller.cpp
@@ -25,12 +25,10 @@ PPMReader ppm(interruptPin, channelAmount);
 
  unsigned THROTTLE = 0; 
  unsigned YAW = 0; 
- unsigned ROLL =  0;
+ unsigned ROLL =  0; 
  unsigned PITCH = 0; 
 
  int start = 0; //Flag to indicate whether to start
-
- int previousTHROTTLE = 0; 
 
 void setup() {
     Serial.begin(115200);
@@ -56,7 +54,7 @@ void loop() {
   ROLL = ppm.rawChannelValue(1); // Right joystick Left and right 
   PITCH =  ppm.rawChannelValue(3); // Right joystick Up and down 
 
-  Serial.print("\n");
+  Serial.println();
   Serial.print("THROTTLE: " + String(THROTTLE) + "\n");
   Serial.print("YAW: " + String(YAW) + "\n");
   Serial.print("PITCH: " + String(PITCH) + "\n");
@@ -75,56 +73,18 @@ void loop() {
     int motd = THROTTLE + YAW - PITCH + ROLL;
 
     // Scale the output values to the range of MIN_PULSE_LENGTH to MAX_PULSE_LENGTH
-    mota = map(mota, 0, 100, MAX_PULSE_LENGTH, MIN_PULSE_LENGTH);
-    motb = map(motb, 0, 100, MAX_PULSE_LENGTH, MIN_PULSE_LENGTH);
+    mota = map(mota, 0, 100, MAX_PULSE_LENGTH, MIN_PULSE_LENGTH); 
+    motb = map(motb, 0, 100, MAX_PULSE_LENGTH, MIN_PULSE_LENGTH); 
     motc = map(motc, 0, 100, MAX_PULSE_LENGTH, MIN_PULSE_LENGTH);
     motd = map(motd, 0, 100, MAX_PULSE_LENGTH, MIN_PULSE_LENGTH);
 
-    // Write the output values to the motors
-    motA.writeMicroseconds(mota);
-    motB.writeMicroseconds(motb);
-    motC.writeMicroseconds(motc);
-    motD.writeMicroseconds(motd);
+    // Updated which propellers are CCW || CW
+    motA.writeMicroseconds(motb); //CCW 
+    motB.writeMicroseconds(motd); //CW
+    motC.writeMicroseconds(mota); //CW
+    motD.writeMicroseconds(motc); //CCW
     Serial.println();
     Serial.print("motA: "); Serial.println(mota); Serial.print("motB: "); Serial.println(motb); Serial.print("motC: "); Serial.println(motc); Serial.print("motD: "); Serial.println(motd);
-
-
-    // int16_t motor3Speed = map(THROTTLE, 1000, 1988, MAX_PULSE_LENGTH, MIN_PULSE_LENGTH); // Reversed MAX & MIN_PULSE_LENGTH...
-    // Serial.print("MOTOR SPEED: ");
-    // Serial.println(motor3Speed); 
-    // motA.writeMicroseconds(motor3Speed); 
-    // motB.writeMicroseconds(motor3Speed); 
-    // motC.writeMicroseconds(motor3Speed); 
-    // motD.writeMicroseconds(motor3Speed); 
-
-  // code below does not work properly
-  // if(THROTTLE < 1493 && THROTTLE > 1485 && start == 0) // Hovering mode (neutral pos of joystick)
-  // {
-  //   motA.writeMicroseconds(MID_PULSE_LENGTH); 
-  //   motB.writeMicroseconds(MID_PULSE_LENGTH); 
-  //   motC.writeMicroseconds(MID_PULSE_LENGTH); 
-  //   motD.writeMicroseconds(MID_PULSE_LENGTH); 
-  //   Serial.println(start); 
-  // }
-  // if(THROTTLE > 1800 && YAW > 1700 && start == 1) //turn off motors 
-  // {
-  //   // Turn off motors. 
-  //   motA.writeMicroseconds(MIN_PULSE_LENGTH); 
-  //   motB.writeMicroseconds(MIN_PULSE_LENGTH); 
-  //   motC.writeMicroseconds(MIN_PULSE_LENGTH); 
-  //   motD.writeMicroseconds(MIN_PULSE_LENGTH); 
-  //   Serial.print("Motors OFF ");
-  //   Serial.println(start);
-  // }
-  // else{ // Increase THROTTLE
-  //   Serial.print("MOTOR SPEED: ");
-  //   Serial.println(motor3Speed); 
-  //   motA.writeMicroseconds(motor3Speed); 
-  //   motB.writeMicroseconds(motor3Speed); 
-  //   motC.writeMicroseconds(motor3Speed); 
-  //   motD.writeMicroseconds(motor3Speed); 
-  //   Serial.println(start); 
-  // }
-    
+   
     delay(500);
 }
