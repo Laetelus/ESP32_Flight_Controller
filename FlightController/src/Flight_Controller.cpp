95182ceaf487ab60a9ac72a380d17ca7fa04ee08
Laetelus <72906227+Laetelus@users.noreply.github.com>
2023-02-22 23:24:10 -0500

    Removed PID_V1 library. Implemented custom version. Throttle functionality working

--- FlightController/src/Flight_Controller.cpp
+++ FlightController/src/Flight_Controller.cpp
@@ -1,20 +1,11 @@
 #include <PPMReader.h>
 #include <ESP32Servo.h> 
-#include <PID_v1.h>
 #include "I2Cdev.h"
 #include "MPU6050.h"
-
-
-// Initialize a PPMReader on digital pin 3 with 8 expected channels.
+// Initialize a PPMReader on digital pin 3 with 6 expected channels.
 byte interruptPin = 13; // PPM pin connector 
 byte channelAmount = 4; // Number of channels to use 
 
-MPU6050 accelgyro; 
-
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
-#define OUTPUT_READABLE_ACCELGYRO
 // Define motor pins 
 #define MOTOR_1_PIN 14
 #define MOTOR_2_PIN 27
@@ -28,30 +19,28 @@ int16_t gx, gy, gz;
 Servo motA,motB,motC,motD; 
 PPMReader ppm(interruptPin, channelAmount);
 
-//Declare variables for PID controllers
-double throttleSetpoint = 0, throttleInput, throttleOutput;
-double yawSetpoint = 0, yawInput, yawOutput;
-double rollSetpoint = 0, rollInput, rollOutput;
-double pitchSetpoint = 0, pitchInput, pitchOutput;
+#define PID_PITCH_P   0
+#define PID_PITCH_D   0
+#define PID_PITCH_I   0
 
-//Do not exceed kp > 2, ki, > 0.5, kd > 2  
-double kp_pitch = 0 , ki_pitch = 0, kd_pitch= 0; 
-double kp_roll = 0,  ki_roll= 0, kd_roll = 0; 
-double kp_yaw = 0, ki_yaw = 0, kd_yaw = 0;   
+#define PID_ROLL_P    0
+#define PID_ROLL_D    0
+#define PID_ROLL_I    0
 
+#define PID_YAW_P     0
+#define PID_YAW_D     0
+#define PID_YAW_I     0
 
+MPU6050 accelgyro; 
 
-PID throttlePID(&throttleInput, &throttleOutput, &throttleSetpoint, kp_pitch, ki_pitch, kd_pitch, DIRECT);
-PID yawPID(&yawInput, &yawOutput, &yawSetpoint, kp_yaw, ki_yaw, kd_yaw, DIRECT);
-PID rollPID(&rollInput, &rollOutput, &rollSetpoint, kp_roll, ki_roll, kd_roll, DIRECT);
-PID pitchPID(&pitchInput, &pitchOutput, &pitchSetpoint, kp_pitch, ki_pitch, kd_pitch, DIRECT);
-
-void IMU_DATA();
+int16_t inline ExecutePitchPID(const int16_t& pitch_set_point, const int16_t& measured_pitch);
+int16_t inline ExecuteRollPID(const int16_t& roll_set_point, const int16_t& measured_roll);
+int16_t inline ExecuteYawPID(const int16_t& yaw_set_point, const int16_t& measured_yaw); 
+void inline MeasurePitchRollYaw(int16_t& measured_pitch, int16_t& measured_roll, int16_t& measured_yaw);
 
 void setup() {
-
     Serial.begin(115200);
-
+    
     // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
@@ -80,96 +69,118 @@ void setup() {
     motC.writeMicroseconds(MIN_PULSE_LENGTH); 
     motD.writeMicroseconds(MIN_PULSE_LENGTH); 
 
-    //Initialize the PID controllers
-    throttlePID.SetMode(AUTOMATIC);
-    yawPID.SetMode(AUTOMATIC);
-    rollPID.SetMode(AUTOMATIC);
-    pitchPID.SetMode(AUTOMATIC);
-        // Set the sample time
-    int16_t sample_time = 50; // in milliseconds
-    throttlePID.SetSampleTime(sample_time);
-    rollPID.SetSampleTime(sample_time);
-    pitchPID.SetSampleTime(sample_time);
-    yawPID.SetSampleTime(sample_time);
-
-    //set the output limits
-    // throttlePID.SetOutputLimits(MIN_PULSE_LENGTH,MAX_PULSE_LENGTH);
-    // rollPID.SetOutputLimits(MIN_PULSE_LENGTH,MAX_PULSE_LENGTH); 
-    // pitchPID.SetOutputLimits(MIN_PULSE_LENGTH,MAX_PULSE_LENGTH); 
-    // yawPID.SetOutputLimits(MIN_PULSE_LENGTH,MAX_PULSE_LENGTH); 
 }
 
 void loop() {
-    //Read the raw channel values
-    int16_t throttle = ppm.rawChannelValue(3);
-    int16_t yaw = ppm.rawChannelValue(4);
-    int16_t roll = ppm.rawChannelValue(1);
-    int16_t pitch = ppm.rawChannelValue(2);
-    
-    pitch = static_cast<int16_t>(accelgyro.getRotationX());
-    roll  = static_cast<int16_t>(accelgyro.getRotationY());
-    yaw  = static_cast<int16_t>(accelgyro.getRotationZ());
 
-    // Scale the input values to a range of 0 to 100
-    throttleInput = map(throttle, 1000, 2000, 0, 100);
-    yawInput = map(yaw, 1000, 2000, -80, 80);
-    Serial.println(yawInput); //yaw input depends on imu... test 
-    rollInput = map(roll, 1000, 2000, -50, 50);
-    pitchInput = map(pitch, 1000, 2000, -50, 50);
- 
-    //Mix the inputs to determine the speed and direction of each motor
-    int16_t mota = map(throttleInput + pitchOutput - rollOutput + yawOutput, 0, 100, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH); //FR
-    int16_t motb = map(throttleInput - pitchOutput - rollOutput - yawOutput, 0, 100, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH); //BR
-    int16_t motc = map(throttleInput + pitchOutput + rollOutput - yawOutput, 0, 100, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH); //FL
-    int16_t motd = map(throttleInput - pitchOutput + rollOutput + yawOutput, 0, 100, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH); //BL
+  //Read the raw channel values
+  int16_t THROTTLE = ppm.rawChannelValue(3);
+  int16_t YAW = ppm.rawChannelValue(4);
+  int16_t ROLL = ppm.rawChannelValue(1);
+  int16_t PITCH = ppm.rawChannelValue(2);
+  
+  int16_t   measured_pitch  = 0;
+  int16_t   measured_roll   = 0;
+  int16_t   measured_yaw    = 0;
 
-    //Call the Compute function for each PID controller
-    throttlePID.Compute();
-    yawPID.Compute();
-    rollPID.Compute();
-    pitchPID.Compute();
-    IMU_DATA(); 
+  // Serial.println();
+  // Serial.print("THROTTLE: " + String(THROTTLE) + "\n");
+  // Serial.print("YAW: " + String(YAW) + "\n");
+  // Serial.print("PITCH: " + String(PITCH) + "\n");
+  // Serial.print("ROLL: " + String(ROLL) + "\n");
+
+
+      // Scale the input values to a range of 0 to 100
+    THROTTLE = map(THROTTLE, 1000, 2000, 0, 100);
+    YAW = map(YAW, 1000, 2000, -80, 80);
+    PITCH = map(PITCH, 1000, 2000, -50, 50);
+    ROLL = map(ROLL, 1000, 2000, -50, 50);
+        
+  MeasurePitchRollYaw(measured_pitch, measured_roll, measured_yaw);
+  
+  int16_t PitchPIDOutput  = ExecutePitchPID(YAW, measured_pitch);
+  int16_t RollPIDOutput   = ExecuteRollPID(PITCH, measured_roll);
+  int16_t YawPIDOutput    = ExecuteYawPID(ROLL, measured_yaw);
+
+    // Mix the input values to determine the speed and direction of each motor
+    int16_t mota = map(THROTTLE + PitchPIDOutput - RollPIDOutput + YawPIDOutput, 0, 100, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH); //FR
+    int16_t motb = map(THROTTLE - PitchPIDOutput - RollPIDOutput - YawPIDOutput, 0, 100, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH); //BR
+    int16_t motc = map(THROTTLE + PitchPIDOutput + RollPIDOutput - YawPIDOutput, 0, 100, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH); //FL
+    int16_t motd = map(THROTTLE - PitchPIDOutput + RollPIDOutput + YawPIDOutput, 0, 100, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH); //BL
     
     Serial.println();
     Serial.print("motA: "); Serial.println(mota); Serial.print("motB: "); Serial.println(motb); Serial.print("motC: "); Serial.println(motc); Serial.print("motD: "); Serial.println(motd);
-
-    // Write the motor outputs
-    motA.writeMicroseconds(mota);
-    motB.writeMicroseconds(motb);
-    motC.writeMicroseconds(motc);
-    motD.writeMicroseconds(motd);
-    delay(500); 
-
+    
+    motA.writeMicroseconds(mota); // CCW 
+    motB.writeMicroseconds(motb); // CW 
+    motC.writeMicroseconds(motc); // CW
+    motD.writeMicroseconds(motd); // CCW
+        
+    delay(500);
 }
 
 
+int16_t inline ExecutePitchPID(const int16_t& pitch_set_point, const int16_t& measured_pitch) {
+  float error;
+  static float previous_error = .0;
+  static float integral = .0;
+  
+  float proportional;
+  float derivative;
+  
+  error = static_cast<float>(pitch_set_point - measured_pitch);
+  
+  proportional = PID_PITCH_P * error;
+  derivative = PID_PITCH_D * (error - (previous_error));
+  integral += PID_PITCH_I * (previous_error + error)/2;
+    
+  previous_error = error;
+  
+  return static_cast<int16_t>(proportional + integral + derivative);
+}
 
-void IMU_DATA(){ 
-#ifdef DEBUG
-        // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+int16_t inline ExecuteRollPID(const int16_t& roll_set_point, const int16_t& measured_roll) {
+  float error;
+  static float previous_error = .0;
+  static float integral = .0;
+  
+  float proportional;
+  float derivative;
+  
+  error = roll_set_point - measured_roll;
+  
+  proportional = PID_ROLL_P * error;
+  derivative = PID_ROLL_D * (error - (previous_error));
+  integral += PID_ROLL_I * (previous_error + error)/2;
+  
+  previous_error = error;
+  
+  return proportional + integral + derivative;
+}
 
-    accelgyro.getAcceleration(&ax, &ay, &az);
-    accelgyro.getRotation(&gx, &gy, &gz);
+int16_t inline ExecuteYawPID(const int16_t& yaw_set_point, const int16_t& measured_yaw) {
+  float error;
+  static float previous_error = .0;
+  static float integral = .0;
+  
+  float proportional;
+  float derivative;
+  
+  error = yaw_set_point - measured_yaw;
+  
+  proportional = PID_YAW_P * error;
+  derivative = PID_YAW_D * (error - (previous_error));
+  integral += PID_YAW_I * (previous_error + error)/2;
+  
+    
+  previous_error = error;
 
-    #ifdef OUTPUT_READABLE_ACCELGYRO
-        // display tab-separated accel/gyro x/y/z values
-        Serial.print("a/g:\t");
-        Serial.print(ax); Serial.print("\t");
-        Serial.print(ay); Serial.print("\t");
-        Serial.print(az); Serial.print("\t");
-        Serial.print(gx); Serial.print("\t");
-        Serial.print(gy); Serial.print("\t");
-        Serial.println(gz);
-    #endif
+  return proportional + integral + derivative;
+}
 
-    #ifdef OUTPUT_BINARY_ACCELGYRO
-        Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
-        Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
-        Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
-        Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
-        Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
-        Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
-    #endif
-#endif
+void inline MeasurePitchRollYaw(int16_t& measured_pitch, int16_t& measured_roll, int16_t& measured_yaw) {
+  measured_pitch = static_cast<int16_t>(accelgyro.getRotationX());
+  Serial.print(measured_pitch);
+  measured_roll =  static_cast<int16_t>(accelgyro.getRotationY());
+  measured_yaw =   static_cast<int16_t>(accelgyro.getRotationZ());
 }
